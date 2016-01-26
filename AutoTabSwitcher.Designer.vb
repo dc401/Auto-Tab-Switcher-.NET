9895ae60b35063f2ad04ad9274b8b2834eff7723
dc401 <dchow@xtecsystems.com>
2016-01-26 07:54:22 -0600

    Auto Tab Switcher .NET
    Uses .NET in VB via SendKeys method and focuses between two applications
    that is called via the shell method within an interval.

---
+++ AutoTabSwitcher.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainMenu
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StartToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StartButton = New System.Windows.Forms.Button()
+        Me.SecondsLabel = New System.Windows.Forms.Label()
+        Me.NumberBox = New System.Windows.Forms.NumericUpDown()
+        Me.BrowseFile1Button = New System.Windows.Forms.Button()
+        Me.BrowseFile2Button = New System.Windows.Forms.Button()
+        Me.BrowseFile1Textbox = New System.Windows.Forms.TextBox()
+        Me.BrowseFile2TextBox = New System.Windows.Forms.TextBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.OpenFileDialog2 = New System.Windows.Forms.OpenFileDialog()
+        Me.HowToToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.NumberBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(390, 331)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 23)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(486, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.StartToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'StartToolStripMenuItem
+        '
+        Me.StartToolStripMenuItem.Name = "StartToolStripMenuItem"
+        Me.StartToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.StartToolStripMenuItem.Text = "Start"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(98, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HowToToolStripMenuItem, Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'StartButton
+        '
+        Me.StartButton.Location = New System.Drawing.Point(390, 261)
+        Me.StartButton.Name = "StartButton"
+        Me.StartButton.Size = New System.Drawing.Size(75, 52)
+        Me.StartButton.TabIndex = 2
+        Me.StartButton.Text = "Start"
+        Me.StartButton.UseVisualStyleBackColor = True
+        '
+        'SecondsLabel
+        '
+        Me.SecondsLabel.AutoSize = True
+        Me.SecondsLabel.Location = New System.Drawing.Point(12, 46)
+        Me.SecondsLabel.Name = "SecondsLabel"
+        Me.SecondsLabel.Size = New System.Drawing.Size(143, 13)
+        Me.SecondsLabel.TabIndex = 4
+        Me.SecondsLabel.Text = "Seconds Between Switching"
+        '
+        'NumberBox
+        '
+        Me.NumberBox.Location = New System.Drawing.Point(12, 62)
+        Me.NumberBox.Maximum = New Decimal(New Integer() {1000, 0, 0, 0})
+        Me.NumberBox.Name = "NumberBox"
+        Me.NumberBox.Size = New System.Drawing.Size(140, 20)
+        Me.NumberBox.TabIndex = 5
+        Me.NumberBox.Value = New Decimal(New Integer() {10, 0, 0, 0})
+        '
+        'BrowseFile1Button
+        '
+        Me.BrowseFile1Button.Location = New System.Drawing.Point(12, 115)
+        Me.BrowseFile1Button.Name = "BrowseFile1Button"
+        Me.BrowseFile1Button.Size = New System.Drawing.Size(128, 31)
+        Me.BrowseFile1Button.TabIndex = 6
+        Me.BrowseFile1Button.Text = "Browse Application 1"
+        Me.BrowseFile1Button.UseVisualStyleBackColor = True
+        '
+        'BrowseFile2Button
+        '
+        Me.BrowseFile2Button.Location = New System.Drawing.Point(12, 173)
+        Me.BrowseFile2Button.Name = "BrowseFile2Button"
+        Me.BrowseFile2Button.Size = New System.Drawing.Size(128, 31)
+        Me.BrowseFile2Button.TabIndex = 7
+        Me.BrowseFile2Button.Text = "Browse Application 2"
+        Me.BrowseFile2Button.UseVisualStyleBackColor = True
+        '
+        'BrowseFile1Textbox
+        '
+        Me.BrowseFile1Textbox.Location = New System.Drawing.Point(146, 126)
+        Me.BrowseFile1Textbox.Name = "BrowseFile1Textbox"
+        Me.BrowseFile1Textbox.Size = New System.Drawing.Size(263, 20)
+        Me.BrowseFile1Textbox.TabIndex = 8
+        '
+        'BrowseFile2TextBox
+        '
+        Me.BrowseFile2TextBox.Location = New System.Drawing.Point(146, 184)
+        Me.BrowseFile2TextBox.Name = "BrowseFile2TextBox"
+        Me.BrowseFile2TextBox.Size = New System.Drawing.Size(263, 20)
+        Me.BrowseFile2TextBox.TabIndex = 9
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'OpenFileDialog2
+        '
+        Me.OpenFileDialog2.FileName = "OpenFileDialog2"
+        '
+        'HowToToolStripMenuItem
+        '
+        Me.HowToToolStripMenuItem.Name = "HowToToolStripMenuItem"
+        Me.HowToToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.HowToToolStripMenuItem.Text = "How To"
+        '
+        'MainMenu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(486, 366)
+        Me.Controls.Add(Me.BrowseFile2TextBox)
+        Me.Controls.Add(Me.BrowseFile1Textbox)
+        Me.Controls.Add(Me.BrowseFile2Button)
+        Me.Controls.Add(Me.BrowseFile1Button)
+        Me.Controls.Add(Me.NumberBox)
+        Me.Controls.Add(Me.SecondsLabel)
+        Me.Controls.Add(Me.StartButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MainMenu"
+        Me.Text = "Auto Tab Switcher"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.NumberBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StartToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents StartButton As Button
+    Friend WithEvents SecondsLabel As Label
+    Friend WithEvents NumberBox As NumericUpDown
+    Friend WithEvents BrowseFile1Button As Button
+    Friend WithEvents BrowseFile2Button As Button
+    Friend WithEvents BrowseFile1Textbox As TextBox
+    Friend WithEvents BrowseFile2TextBox As TextBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents OpenFileDialog2 As OpenFileDialog
+    Friend WithEvents HowToToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ AutoTabSwitcher.vb
@@ -0,0 +1,177 @@
+﻿'Auto switches between programs on the desktop on Windows
+'dchow[AT]xtecsystems.com 20160125
+'Version 1.0
+'Import some shit
+Imports System.Threading
+Imports System.IO
+
+Public Class MainMenu
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Close()
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Close()
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        MessageBox.Show("This program switches between programs based on interval using alt tab and focus. 20160125 dchow")
+    End Sub
+
+    Private Sub StartButton_Click(sender As Object, e As EventArgs) Handles StartButton.Click
+        'Loop the send key forever
+        Dim fookMe As Short
+        fookMe = 0
+
+        Dim msValue As Integer
+        msValue = (NumberBox.Value)
+        Dim secondsValue As Integer
+        secondsValue = (msValue * 1000)
+
+        'Store pids opening the browser and server manager
+        'Dim pidBrowser As Integer
+        'Dim pidServerManager As Integer
+        'Dim pidNotepad As Integer
+        'Dim pidsmLauncher As Integer
+
+        'load up the shell env variables
+        'Dim programFilesDir As String
+        'Dim system32Dir As String
+        'Dim iexplore As String
+        'Dim serverManager As String
+        'Dim notepad As String
+
+        'programFilesDir = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles)
+        'iexplore = programFilesDir + "\" + "Internet Explorer\" + "iexplore.exe"
+
+        'system32Dir = Environment.GetFolderPath(Environment.SpecialFolder.System)
+        'serverManager = system32Dir + "\" + "ServerManager.exe"
+        'notepad = system32Dir + "\" + "notepad.exe"
+
+        'Input from OpenFileDialog boxes
+        Dim app1Path = BrowseFile1Textbox.Text.ToString()
+        Dim app2Path = BrowseFile2TextBox.Text.ToString()
+
+        Dim pidApp1 As Integer
+        Dim pidApp2 As Integer
+
+        'launch the apps and store the pids
+        'pidBrowser = Shell(iexplore, AppWinStyle.NormalFocus)
+        'Servermanager.exe refuses to run despite good path. Possible UAC or Escalation issues. Fucking bull shit.
+        'http://forums.codeguru.com/printthread.php?t=347845&pp=50
+        'pidNotepad = Shell(notepad, AppWinStyle.NormalNoFocus)
+
+        pidApp1 = Shell(app1Path, AppWinStyle.NormalFocus)
+        'pidApp1 = Shell("C:\Windows\system32\notepad.exe", AppWinStyle.NormalFocus)
+        'Apps may open too fast during focus before activation
+        Thread.Sleep(1000)
+        pidApp2 = Shell(app2Path, AppWinStyle.NormalNoFocus)
+
+        Do While fookMe < 1
+            Try
+                AppActivate(pidApp1)
+                'Using browser pid first seems to crash upon alt tab for unknown reason
+                AppActivate(pidApp1)
+                Thread.Sleep(secondsValue)
+                SendKeys.SendWait("%{TAB}")
+                'SendKeys.SendWait("{F2}")
+                Thread.Sleep(secondsValue)
+                'SendKeys.SendWait("{F1}")
+            Catch
+                MessageBox.Show("An Error Has Occured. Quitting.")
+                Close()
+            End Try
+
+        Loop
+    End Sub
+
+    Private Sub NumberBox_ValueChanged(sender As Object, e As EventArgs) Handles NumberBox.ValueChanged
+
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles BrowseFile1Button.Click
+        OpenFileDialog1.Title = "Please Select a File"
+        OpenFileDialog1.InitialDirectory = "C:\"
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk_1(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        'Jacked this guys code: http://www.techrepublic.com/blog/software-engineer/let-users-pick-a-file-to-open-via-openfiledialog-in-vbnet/
+        Dim strm As System.IO.Stream
+        strm = OpenFileDialog1.OpenFile()
+        BrowseFile1Textbox.Text = OpenFileDialog1.FileName.ToString()
+    End Sub
+
+    Private Sub BrowseFile2Button_Click(sender As Object, e As EventArgs) Handles BrowseFile2Button.Click
+        OpenFileDialog2.Title = "Please Select a File"
+        OpenFileDialog2.InitialDirectory = "C:\"
+        OpenFileDialog2.ShowDialog()
+    End Sub
+
+    Private Sub OpenFileDialog2_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog2.FileOk
+        Dim strm As System.IO.Stream
+        strm = OpenFileDialog2.OpenFile()
+        BrowseFile2TextBox.Text = OpenFileDialog2.FileName.ToString()
+    End Sub
+
+    Private Sub HowToToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HowToToolStripMenuItem.Click
+        MessageBox.Show("Browse to the first application you want to ALT+TAB between and then do the same for the second using the buttons." +
+                        Environment.NewLine + "Select the timer to the number of seconds between intervals." +
+                        Environment.NewLine + "Some programs work better than others. Best to use in Windows 7 or below." +
+                        Environment.NewLine + "Use of processes that have multiple focus input areas tend to cause the most trouble like IE" +
+                        Environment.NewLine + "Also note if .NET has no visibility or has UAC permission issues, it will fail to load the app" +
+                        Environment.NewLine +
+                        Environment.NewLine + "If you still can't figure that out, just press ALT+F4 now. dchow[AT]xtecsystems.com ")
+    End Sub
+
+    Private Sub StartToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles StartToolStripMenuItem.Click
+        'Variables for the loop
+        Dim fookMe As Short
+        fookMe = 0
+
+        'Convert miliseconds to seconds
+        Dim msValue As Integer
+        msValue = (NumberBox.Value)
+        Dim secondsValue As Integer
+        secondsValue = (msValue * 1000)
+
+        'Input from OpenFileDialog boxes
+        Dim app1Path = BrowseFile1Textbox.Text.ToString()
+        Dim app2Path = BrowseFile2TextBox.Text.ToString()
+
+        Dim pidApp1 As Integer
+        Dim pidApp2 As Integer
+
+
+        'Call the external application and set window focus return the process id to integer
+        pidApp1 = Shell(app1Path, AppWinStyle.NormalFocus)
+        'Apps may open too fast during focus before activation
+        Thread.Sleep(1000)
+        pidApp2 = Shell(app2Path, AppWinStyle.NormalNoFocus)
+
+        'Infinite loop until you close one or more of the processes which throws an error to force quit
+        Do While fookMe < 1
+            Try
+                AppActivate(pidApp1)
+                'Using browser pid first seems to crash upon alt tab for unknown reason
+                AppActivate(pidApp1)
+                Thread.Sleep(secondsValue)
+                SendKeys.SendWait("%{TAB}")
+                'SendKeys.SendWait("{F2}")
+                Thread.Sleep(secondsValue)
+                'SendKeys.SendWait("{F1}")
+            Catch
+                MessageBox.Show("An Error Has Occured. Quitting.")
+                Close()
+            End Try
+
+        Loop
+    End Sub
+End Class
